79529640d11b88710ae45215dff76dcc3ddc7ae9
Farid Aliev <farid-keltuzed@mail.ru>
2024-09-12 21:24:32 +0300

    .

--- src/locales/ru.js
+++ src/locales/ru.js
@@ -1,23 +1,23 @@
 export default {
-    translation: {
-      languages: {
-        en: 'English',
-        ru: 'Русский',
-      },
-      messages: {
-        success: 'RSS успешно загружен',
-        errors: {
-          invalidURL: 'Ссылка должна быть валидным URL',
-          alreadyExists: 'RSS уже существует',
-          networkError: 'Ошибка сети',
-          parsingError: 'Ресурс не содержит валидный RSS',
-          updateError: 'Ошибка обновления ленты постов',
-        },
-      },
-      ui: {
-        feeds: 'Фиды',
-        posts: 'Посты',
-        button: 'Просмотр',
+  translation: {
+    languages: {
+      en: 'English',
+      ru: 'Русский',
+    },
+    messages: {
+      success: 'RSS успешно загружен',
+      errors: {
+        invalidURL: 'Ссылка должна быть валидным URL',
+        alreadyExists: 'RSS уже существует',
+        networkError: 'Ошибка сети',
+        parsingError: 'Ресурс не содержит валидный RSS',
+        updateError: 'Ошибка обновления ленты постов',
       },
     },
-  };
+    ui: {
+      feeds: 'Фиды',
+      posts: 'Посты',
+      button: 'Просмотр',
+    },
+  },
+};
--- src/parser.js
+++ src/parser.js
@@ -1,31 +1,31 @@
 const getFeedData = (document) => {
-    const feedData = {
-      title: document.querySelector('title').textContent,
-      description: document.querySelector('description').textContent,
-    };
-    return feedData;
+  const feedData = {
+    title: document.querySelector('title').textContent,
+    description: document.querySelector('description').textContent,
   };
-  
-  const getPostData = (document) => {
-    const items = document.querySelectorAll('item');
-    const postData = [...items].map((item) => ({
-      title: item.querySelector('title').textContent,
-      description: item.querySelector('description').textContent,
-      link: item.querySelector('link').textContent,
-    }));
-    return postData;
+  return feedData;
+};
+
+const getPostData = (document) => {
+  const items = document.querySelectorAll('item');
+  const postData = [...items].map((item) => ({
+    title: item.querySelector('title').textContent,
+    description: item.querySelector('description').textContent,
+    link: item.querySelector('link').textContent,
+  }));
+  return postData;
+};
+
+export default (data) => {
+  const parser = new DOMParser();
+  const parsedXml = parser.parseFromString(data, 'application/xml');
+  if (parsedXml.querySelector('parsererror')) {
+    const error = new Error(parsedXml.querySelector('parsererror').textContent);
+    error.name = 'parsingError';
+    throw error;
+  }
+  return {
+    feed: getFeedData(parsedXml),
+    posts: getPostData(parsedXml),
   };
-  
-  export default (data) => {
-    const parser = new DOMParser();
-    const parsedXml = parser.parseFromString(data, 'application/xml');
-    if (parsedXml.querySelector('parsererror')) {
-      const error = new Error(parsedXml.querySelector('parsererror').textContent);
-      error.name = 'parsingError';
-      throw error;
-    }
-    return {
-      feed: getFeedData(parsedXml),
-      posts: getPostData(parsedXml),
-    };
-  };
+};
